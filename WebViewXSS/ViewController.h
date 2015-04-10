affc93444155034e2332088989df6f20ff099c91
Oliver N <oliverng@gmail.com>
2015-04-10 13:27:05 +0800

    Final version of UIWebView XSS testing and testing for SOP using embedded WebViews and loadRequest vs. loadHTMLString for local files

--- WebViewXSS/ViewController.h
+++ WebViewXSS/ViewController.h
@@ -10,6 +10,16 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
+@property (weak, nonatomic) IBOutlet UITextField *nameTxtFld;
+
+- (IBAction)loadWebViewLocal:(id)sender;
+- (IBAction)loadWebViewInternet:(id)sender;
+- (IBAction)testXSS:(id)sender;
+- (IBAction)testSOPAppleWeb:(id)sender;
+- (IBAction)testSOPAboutBlank:(id)sender;
+- (IBAction)testSOPFromBundle:(id)sender;
+- (IBAction)loadSafariInternet:(id)sender;
 
 @end
 
